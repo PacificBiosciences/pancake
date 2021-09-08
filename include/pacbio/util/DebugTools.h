590313d093e21bca053cdeeed11280027171c692
Ivan Sovic <ivan.sovic@gmail.com>
2021-09-08 14:24:46 +0200

    Added include/pacbio/util/DebugTools.h.

---
+++ include/pacbio/util/DebugTools.h
@@ -0,0 +1,52 @@
+// Author: Ivan Sovic
+
+#ifndef PANCAKE_DEBUG_TOOLS_H
+#define PANCAKE_DEBUG_TOOLS_H
+
+#include <pacbio/util/TicToc.h>
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+
+namespace PacBio {
+namespace Pancake {
+
+#ifdef DEBUG_LOG_TIMINGS
+inline void LogTicToc(const std::string& label, TicToc& tt,
+                      std::unordered_map<std::string, double>& timings)
+{
+    tt.Stop();
+    timings[label] = tt.GetMicrosecs();
+    tt.Start();
+}
+inline void LogTicTocAdd(const std::string& label, TicToc& tt,
+                         std::unordered_map<std::string, double>& timings)
+{
+    tt.Stop();
+    timings[label] += tt.GetMicrosecs();
+    tt.Start();
+}
+inline void LogTicTocAdd(const std::string& label, const double timeToAdd,
+                         std::unordered_map<std::string, double>& timings)
+{
+    timings[label] += timeToAdd;
+}
+#else
+inline void LogTicToc(const std::string& label, TicToc& tt,
+                      std::unordered_map<std::string, double>& timings)
+{
+}
+inline void LogTicTocAdd(const std::string& label, TicToc& tt,
+                         std::unordered_map<std::string, double>& timings)
+{
+}
+inline void LogTicTocAdd(const std::string& label, const double timeToAdd,
+                         std::unordered_map<std::string, double>& timings)
+{
+}
+#endif
+
+}  // namespace Pancake
+}  // namespace PacBio
+
+#endif  // PANCAKE_COMMON_H
